c56720fd381bf93fd88c089344bf2493d7908fa0
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-10-08 10:31:56 -0700

    added podfiles, added firebase, test data to firebase database working

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -5,8 +5,8 @@
 //  The App Delegate listens for events from the system. 
 //  It recieves application level messages like did the app finish launching or did it terminate etc. 
 //
-
 import UIKit
+import Firebase
 
 
 @UIApplicationMain
@@ -18,7 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         //TODO: Initialise and Configure your Firebase here:
-        
+        FirebaseApp.configure()
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("We've Got Data")
         
         return true
     }
